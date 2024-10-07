570b91f7ff934f5633a59245e8d62259ec412321
jathin kalyanam <jathinkalyanam@jathins-Laptop.local>
2024-10-07 15:10:18 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import Topbar from './components/topbar/Topbar';
+import Footer from './components/footer/Footer.jsx';
+import Home from './components/pages/home/Home';
+import About from './components/pages/about/About.jsx';
+import Contact from './components/pages/Contact/Contact.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Topbar />
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
